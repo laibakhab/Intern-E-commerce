c2b2e26cb462026141d154481bf59df741976e59
laibakhab <asifalikhan4485@gmail.com>
2025-08-13 00:21:09 +0500

    finall

--- src/app/thrPage/page.tsx
+++ src/app/thrPage/page.tsx
@@ -342,7 +342,7 @@ export default function ProductListing() {
                       src={product.image || "/placeholder.svg"}
                       alt={product.name}
                       width={200}
-                      height={100}
+                      height={100} 
                       className=" h-50 object-cover rounded-lg"
                     />
                     <Button variant="ghost" size="icon" className="absolute top-2 right-2 bg-white/80 hover:bg-white">
